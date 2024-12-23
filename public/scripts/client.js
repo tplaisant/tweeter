b925fad048b3af8ffc7b89b6763556d2d97cda2e
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-12-22 22:44:31 -0700

    adding dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,26 +3,80 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweet = $(`<article class="tweet">Hello world</article>`);
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
 
-const createTweetElement = (tweet) => {
-  let result = $(`<article class="tweet">Hello world</article>`);
-  return result;
+$(document).ready(()=> {
+  
+  const createTweetElement = function(tweet) {
+
+    return $(`<article class="tweet-text">
+      <header>
+        <span class="user-img">      
+          <img src="${tweet.user.avatars} class="old-tweets">
+          <span class="user-info">
+            <p>${tweet.user.name}</p>
+            <p>${tweet.user.handle}</p>
+          </span>
+        </span>
+      </header>
+      <section class="tweet-text">
+        ${tweet.content.text}
+      </section>
+      <footer>
+      ${tweet.created_at}
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`
+    );
+  }
+
+const renderTweets = (arrayOfTweets) => {
+
+  for (let tweet of arrayOfTweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+  
 }
 
-const $tweet = createTweetElement(tweet);
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  renderTweets(data);
+})
