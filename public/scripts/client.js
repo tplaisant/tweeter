08b1f9b517e4dbf37d7b473ae607b9b6308a61f9
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-12-23 19:27:01 -0700

    adding dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,31 @@
 
 $(document).ready(()=> {
   
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
   const createTweetElement = function(tweet) {
 
     return $(`<article class="tweet-text">
@@ -33,50 +58,22 @@ $(document).ready(()=> {
     );
   }
 
-const renderTweets = (arrayOfTweets) => {
-
-  for (let tweet of arrayOfTweets) {
-    $('#tweets-container').append(createTweetElement(tweet));
+  const renderTweets = (arrayOfTweets) => {
+    for (let tweet of arrayOfTweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }    
   }
-  
-}
-
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   renderTweets(data);
+
+  let $button = $("button");
+
+  $button.click((event)=> {    
+    event.preventDefault();
+
+    // $.post();
+
+    // $("#tweet-text").val("");
+  })
+
 })
