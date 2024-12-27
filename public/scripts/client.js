dffba53d397f6b15c033e3a93b71a9c4a483059c
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-12-26 20:49:08 -0700

    Cross-Site Scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,18 +23,24 @@ $(document).ready(()=> {
 
     const tweetCreatedAt = timeago.format(tweet.created_at);
 
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+
     return $(`<article class="tweet-text outra">
       <header>
         <span class="user-img">      
-          <img src="${tweet.user.avatars} class="old-tweets">
+          <img src="${escape(tweet.user.avatars)} class="old-tweets">
           <span class="user-info">
-            <p>${tweet.user.name}</p>
-            <p>${tweet.user.handle}</p>
+            <p>${escape(tweet.user.name)}</p>
+            <p>${escape(tweet.user.handle)}</p>
           </span>
         </span>
       </header>
       <section class="tweet-text">
-        ${tweet.content.text}
+        ${escape(tweet.content.text)}
       </section>
       <footer>
       ${tweetCreatedAt}
@@ -72,7 +78,7 @@ $(document).ready(()=> {
 
   $form.on("submit", (event)=> {        
     const formData = $form.serialize();        
-
+    
     event.preventDefault();
     if (isTweetValid()) {
       $.ajax({
@@ -81,7 +87,6 @@ $(document).ready(()=> {
         data: formData,
         success: (response) => {
           $("#tweet-text").val("");
-          // $('#tweets-container').remove();
           $('#tweets-container').empty();
           $('.counter').val(140);
           loadTweets();
